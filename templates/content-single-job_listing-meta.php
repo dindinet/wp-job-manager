5922c3e2cc764fd266510bfe879ad87d72676f7c
dindinet <github.dklongley@neverbox.com>
2015-02-02 21:43:16 +0000

    first commit

---
+++ templates/content-single-job_listing-meta.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Single view Job meta box
+ *
+ * Hooked into single_job_listing_start priority 20
+ *
+ * @since  1.14.0
+ */
+?>
+<?php do_action( 'single_job_listing_meta_before' ); ?>
+
+<ul class="meta">
+	<?php do_action( 'single_job_listing_meta_start' ); ?>
+
+	<li class="job-type <?php echo get_the_job_type() ? sanitize_title( get_the_job_type()->slug ) : ''; ?>" itemprop="employmentType"><?php the_job_type(); ?></li>
+	<li class="job-salary" itemprop="jobSalary"><?php printf(get_post_meta( get_the_ID(), '_job_salary', true )) ?> </li>
+	<li class="location" itemprop="jobLocation"><?php the_job_location(); ?></li>
+
+	<li class="date-posted" itemprop="datePosted"><date><?php printf( __( 'Posted %s ago', 'wp-job-manager' ), human_time_diff( get_post_time( 'U' ), current_time( 'timestamp' ) ) ); ?></date></li>
+
+	
+	<?php if ( is_position_filled() ) : ?>
+		<li class="position-filled"><?php _e( 'This position has been filled', 'wp-job-manager' ); ?></li>
+	<?php endif; ?>
+	<li>
+
+	<?php do_action( 'single_job_listing_meta_end' ); ?>
+</ul>
+
+
+<?php do_action( 'single_job_listing_meta_after' ); ?>
